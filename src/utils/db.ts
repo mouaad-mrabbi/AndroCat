754bc818ee660e0822ecf28855d62dedde3ac4a9
Mouaad Mrabbi <mouadmrabbi1@gmail.com>
2025-04-11 22:53:37 +0100

    رسالة التحديث

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -10,6 +10,7 @@ declare global {
 
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
