89de66ff8204a6e73e950445fc5373c9bc87e499
Mouaad Mrabbi <mouadmrabbi1@gmail.com>
2025-04-20 17:47:43 +0100

    Fix: 1.0.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
+// This project was developed by MOUAAD MRABBI - 2025
+// All rights reserved © AndroCat
+
 export const metadata = {
   title: "AndroCat",
-  description:
-    "Download free games and programs, including many popular Android games with MOD.",
+  description: "Download free games and programs, including many popular Android games with MOD.",
   icons: "/images/logo.png",
+  author: "MOUAAD MRABBI",
+  copyright: "© 2025 AndroCat. All rights reserved.",
 };
 
 import "./globals.css";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import { ToastContainer } from "react-toastify";
+import '@/components/clientLogger';
 
 export default function RootLayout({
   children,
@@ -18,7 +23,7 @@ export default function RootLayout({
       <body>
         <ToastContainer />
         <Nav />
-        <div className="max-w-[1256px] mx-auto">
+        <div className="max-w-[1348px] mx-auto">
           {children}
           <Footer />
         </div>
---
+++ src/components/clientLogger.ts
@@ -0,0 +1,4 @@
+"use client";
+
+console.log("%cThis project was developed by MOUAAD MRABBI - 2025", "color: #4CAF50; font-weight: bold; font-size: 16px; background-color: #333; padding: 5px; border-radius: 5px;");
+console.log("%cAll rights reserved © AndroCat", "color: #FF5733; font-weight: bold; font-size: 16px; background-color: #333; padding: 5px; border-radius: 5px;");
