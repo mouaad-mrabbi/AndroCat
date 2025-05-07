7f3c45362d711f89c33dd51ce96d8648174e4a15
Mouaad Mrabbi <mouadmrabbi1@gmail.com>
2025-05-07 19:39:32 +0100

    Fix: 1.0.31

--- src/app/admin/(profile)/create/FormCreatePArticle.tsx
+++ src/app/admin/(profile)/create/FormCreatePArticle.tsx
@@ -1084,7 +1084,7 @@ const FormCreatePArticle = () => {
               disabled={isLoading}
               type="submit"
               className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 
-              focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-slate-700"
             >
               Create Item
             </button>
--- src/app/admin/(profile)/pendingArticles/update/[pendingArticleId]/FormUPA.tsx
+++ src/app/admin/(profile)/pendingArticles/update/[pendingArticleId]/FormUPA.tsx
@@ -1175,7 +1175,7 @@ export default function FormUPA({ pendingArticleId }: pageProps) {
               disabled={isLoading}
               type="submit"
               className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 
-              focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-slate-700"
             >
               Create Article
             </button>
